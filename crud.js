54318c628862db78be53aac51737665951f9e911
jorgetlp <jorgetlpk2003@gmail.com>
2023-05-17 04:43:12 -0600

    segundo commit

--- crud.js
+++ crud.js
@@ -2,11 +2,25 @@ const formularioT = document.querySelector('#formulario');
 const listaTareas = document.getElementById('lista');
 let arrayTareas = [];
 
+function obtenerUltimoId() {
+    var id = localStorage.getItem("ultimoId");
+    return id ? parseInt(id) : 0;
+  }
+  
+  function guardarUltimoId(id) {
+    localStorage.setItem("ultimoId", id.toString());
+  }
+  
+  function generarId() {
+    var ultimoId = obtenerUltimoId();
+    var nuevoId = ultimoId + 1;
+    guardarUltimoId(nuevoId);
+    return nuevoId;
+  }
+
 const crearTarea = (tarea, descripcion, estatus) => {
     let item = {
-        tarea: tarea,
-        descripcion: descripcion,
-        estatus: estatus
+        id: generarId(), tarea, descripcion, estatus
     }
 
     arrayTareas.push(item);
@@ -14,7 +28,7 @@ const crearTarea = (tarea, descripcion, estatus) => {
     return item;
 }
 
-const Guardar = () => {
+const guardarTarea = () => {
     localStorage.setItem('tarea',JSON.stringify(arrayTareas));
 
     Mostrar();
@@ -29,14 +43,22 @@ const Mostrar = () => {
         arrayTareas = [];
     }else{
         arrayTareas.forEach(element => {
-            listaTareas.innerHTML += `<tr>
-            <td>${element.tarea}</td>
-            <td>${element.descripcion}</td>
-            <td>${element.estatus}</td>
-            <td><i class="fa-solid fa-pen-nib" style="color: green; padding-left: 5px;"></i><i class="fa-solid fa-trash" style="color: red; padding-left: 5px;"></i></td>
-          </tr>`
+            listaTareas.innerHTML += `<div class="alert alert-success" role="alert"><b>${element.id}</b> - ${element.tarea} - ${element.descripcion} - ${element.estatus}<span class="float-right"><i id="${element.id}" class="material-icons" style="color:green; padding-left:20px">edit</i><i id="${element.id}" class="material-icons" style="color:red; padding-left:5px">delete</i></span></div>`
         });
     }
+}
+
+const eliminarTarea = (id) => {
+    let indexArray;
+    arrayTareas.forEach((elemento, index) => {
+
+        if(elemento.id === id){
+            indexArray = index;
+        }
+    });
+
+    arrayTareas.splice(indexArray,1);
+    guardarTarea();
 }
 
 formularioT.addEventListener('submit', (e) => {
@@ -46,9 +68,32 @@ formularioT.addEventListener('submit', (e) => {
     let Descripcion = document.querySelector('#inputDescripcion').value;
     let Estatus = document.querySelector('#inputEstatus').value;
 
+    if(Titulo === "" || Descripcion === ""){
+        alert("Por favor llene todos los campos");
+        return false;
+    }
+
+
     crearTarea(Titulo, Descripcion, Estatus);
-    Guardar();
+    guardarTarea();
     formularioT.reset();
-})
+    
+    
+});
 
 document.addEventListener('DOMContentLoaded', Mostrar);
+
+listaTareas.addEventListener('click', (e) => {
+    e.preventDefault(); 
+
+    if(e.target.innerHTML === 'edit' || e.target.innerHTML == 'delete'){
+        let valorId = e.target.id;
+        if(e.target.innerHTML === 'edit'){
+
+        }
+        if(e.target.innerHTML === 'delete'){
+            eliminarTarea(valorId);
+        }
+    }
+
+});
