ac51b8e1e554884bba7ccb5d28a611071a84b8fc
jorgetlp <jorgetlpk2003@gmail.com>
2023-05-18 02:07:18 -0600

    tercer commit

--- crud.js
+++ crud.js
@@ -1,4 +1,4 @@
-const formularioT = document.querySelector('#formulario');
+const formularioTareas = document.querySelector('#formulario');
 const listaTareas = document.getElementById('lista');
 let arrayTareas = [];
 
@@ -29,7 +29,7 @@ const crearTarea = (tarea, descripcion, estatus) => {
 }
 
 const guardarTarea = () => {
-    localStorage.setItem('tarea',JSON.stringify(arrayTareas));
+    localStorage.setItem('tareaGuardada',JSON.stringify(arrayTareas));
 
     Mostrar();
 }
@@ -37,7 +37,7 @@ const guardarTarea = () => {
 const Mostrar = () => {
     listaTareas.innerHTML = '';
 
-    arrayTareas = JSON.parse(localStorage.getItem('tarea'));
+    arrayTareas = JSON.parse(localStorage.getItem('tareaGuardada'));
     
     if(arrayTareas === null){
         arrayTareas = [];
@@ -61,7 +61,75 @@ const eliminarTarea = (id) => {
     guardarTarea();
 }
 
-formularioT.addEventListener('submit', (e) => {
+function editarTarea(){
+    var nuevaTarea = {id: null, tarea: null, descripcion: null, estatus: null};
+
+    var id2 =  document.querySelector('#inputId').value;
+    let titulo2 = document.querySelector('#inputTitulo').value;
+    let descripcion2 = document.querySelector('#inputDescripcion').value;
+    let estatus2 = document.querySelector('#inputEstatus').value;
+
+    var idInt = parseInt(id2);
+
+    nuevaTarea = {id: idInt, tarea: titulo2, descripcion: descripcion2, estatus: estatus2};
+
+    actualizarRegistro(idInt, nuevaTarea);
+
+    document.getElementById('btnActualizar').style.display = 'none';
+    document.getElementById('btnAgregar').style.display = 'block';
+
+    document.getElementById('labelId').style.display = 'none';
+    document.getElementById('inputId').style.display = 'none';
+
+    formularioTareas.reset();
+}
+
+function actualizarRegistro(id3, nuevoRegistro) {
+
+    var tareasRegistradas = JSON.parse(localStorage.getItem('tareaGuardada')) || [];
+
+    /*console.log(tareasRegistradas);
+    console.log(id3);
+    console.log(nuevoRegistro);*/
+
+    for (var i3 = 0; i3 < tareasRegistradas.length; i3++) {
+      if (tareasRegistradas[i3].id === id3) {
+        tareasRegistradas[i3] = nuevoRegistro;
+        localStorage.setItem('tareaGuardada', JSON.stringify(tareasRegistradas));
+        return true;
+      }
+    }
+  
+    return false;
+  }
+
+function cargarTarea(valorId) {
+
+    let result = {id: null, tarea: null, descripcion: null, estatus: null};
+
+    var arrayTareas2 = JSON.parse(localStorage.getItem('tareaGuardada')) || [];
+
+    for(var i = 0;i < arrayTareas2.length; i++){
+        if(arrayTareas2[i].id == valorId){
+            result = {id: arrayTareas2[i].id, tarea: arrayTareas2[i].tarea, descripcion: arrayTareas2[i].descripcion, estatus: arrayTareas2[i].estatus};
+            break;
+        }
+    }
+
+    formularioTareas.inputId.value = result.id;
+    formularioTareas.inputTitulo.value = result.tarea;
+    formularioTareas.inputDescripcion.value = result.descripcion;
+    formularioTareas.inputEstatus.value = result.estatus;
+
+    document.getElementById('btnActualizar').style.display = 'block';
+    document.getElementById('btnAgregar').style.display = 'none';
+
+    document.getElementById('labelId').style.display = 'block';
+    document.getElementById('inputId').style.display = 'block';
+}
+  
+
+formularioTareas.addEventListener('submit', (e) => {
 
     e.preventDefault();
     let Titulo = document.querySelector('#inputTitulo').value;
@@ -76,8 +144,7 @@ formularioT.addEventListener('submit', (e) => {
 
     crearTarea(Titulo, Descripcion, Estatus);
     guardarTarea();
-    formularioT.reset();
-    
+    formularioTareas.reset();
     
 });
 
@@ -89,7 +156,7 @@ listaTareas.addEventListener('click', (e) => {
     if(e.target.innerHTML === 'edit' || e.target.innerHTML == 'delete'){
         let valorId = e.target.id;
         if(e.target.innerHTML === 'edit'){
-
+            cargarTarea(valorId);
         }
         if(e.target.innerHTML === 'delete'){
             eliminarTarea(valorId);
